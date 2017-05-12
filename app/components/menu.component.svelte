15e9591a4c25d1c6135cf7037209ded181402eb8
Claudio <claudiohw@gmail.com>
2017-05-12 15:56:03 -0300

    Migrating Menu component

---
+++ app/components/menu.component.svelte
@@ -0,0 +1,30 @@
+<div>
+  <div class="MenuComp--overlay" ref:overlay on:click="closeMenu()"></div>
+  <div class="MenuComp" ref:menu>
+    <div class="logo">
+      <img src="img/logo.svg" alt="PeerAssets WhitePaper">
+    </div>
+    <ul class="menu">
+      {{#each menuItems as item}}
+        <li>{{item.label}}</li>
+      {{/each}}
+    </ul>
+  </div>
+</div>
+
+<script>
+  export default {
+    data () {
+      return {
+        menuItems: [],
+        isOpen: false
+      }
+    },
+
+    methods: {
+      closeMenu () {
+        alert(1);
+      }
+    }
+  }
+</script>
