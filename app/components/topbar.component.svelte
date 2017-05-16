223f50da5be90ac960202c5af5909ca5b0f0732a
Claudio <claudiohw@gmail.com>
2017-05-16 15:28:21 -0300

    Importing icons using svg instead of img

--- app/components/topbar.component.svelte
+++ app/components/topbar.component.svelte
@@ -1,50 +1,21 @@
   <div class="TopbarComp">
-    <div class="menu-button" onclick="{ toggleMenu }">
+    <div class="menu-button" onclick="{ fire('mobilemenu', {action: 'TOGGLE'}) }">
       <img class="{ off: parent.refs.menuComp.isOpen }" src="img/icon-menu-open.svg" width="20" alt="Open Menu">
-      <img class="{ off: !parent.refs.menuComp.isOpen }" src="img/icon-menu-close.svg" width="15" alt="Open Menu">
+      <img class="{ off: !parent.refs.menuComp.isOpen }" src="img/icon-menu-close.svg" width="15" alt="Close Menu">
     </div>
     <div class="btn-group">
       <div class="btn" on:click="fire('fontsize', {action: 'DECREASE'})">
-        <img src="img/icon-font-minus.svg" alt="Smaller font">
+        <svg class="ac-svg" role="img" title="Preview Mode" width="22">
+          <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/icon-font-minus.svg"></use>
+        </svg>
+        <img src="" alt="Smaller font">
       </div>
       <div class="btn" on:click="fire('fontsize', {action: 'INCREASE'})">
-        <img src="img/icon-font-plus.svg" alt="Smaller font">
+        <svg class="ac-svg" role="img" title="Preview Mode" width="22">
+          <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/icon-font-plus.svg"></use>
+        </svg>
       </div>
     </div>
 
     <a href="https://github.com/PeerAssets/WhitePaper" class="btn">Visit Github Project</a>
   </div>
-
-<script>
-  export default {
-    methods: {
-      fontMinus () {
-        console.log(1);
-      }
-    }
-  }
-/*
-  fontMinus () {
-    if(this.parent.fontSize > 1) {
-      this.parent.fontSize--;
-      this.parent.refs.docContainer.classList.remove('font-1', 'font-2', 'font-3', 'font-4');
-      this.parent.refs.docContainer.classList.add('font-' + this.parent.fontSize);
-    }
-  }
-
-  fontPlus () {
-    if(this.parent.fontSize < 4) {
-      this.parent.fontSize++;
-      this.parent.refs.docContainer.classList.remove('font-1', 'font-2', 'font-3', 'font-4');
-      this.parent.refs.docContainer.classList.add('font-' + this.parent.fontSize);
-    }
-  }
-
-  toggleMenu () {
-    if(this.parent.refs.menuComp.isOpen) {
-      this.parent.refs.menuComp.close();
-    } else {
-      this.parent.refs.menuComp.open();
-    }
-  }*/
-</script>
