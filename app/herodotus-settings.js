759fe82e5e6637bee36ff40d1aba0959eb27f32d
Claudio <claudiohw@gmail.com>
2017-05-16 21:25:05 -0300

    Making config file empty

--- app/herodotus-settings.js
+++ app/herodotus-settings.js
@@ -2,11 +2,11 @@ export default {
   documentation_name: 'My awesome documentation',
 
   allow_topbar_link: true,
-  topbar_link_label: 'Visit Github Project',
-  topbar_link_url: 'http://google.com',
+  topbar_link_label: 'Visit our website',
+  topbar_link_url: '#',
 
   allow_font_size_control: true,
 
   allow_remote_documentation: false,
-  remote_documentation_url: 'https://svelte.technology/guide#if-blocks'
+  remote_documentation_url: ''
 }
