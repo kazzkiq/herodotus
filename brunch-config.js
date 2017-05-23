dc875eb6bc6b765515f16154b6171775fe32a143
Kazzkiq <claudiohw@gmail.com>
2017-05-23 01:27:28 -0300

    Fixing production code optimizer

--- brunch-config.js
+++ brunch-config.js
@@ -1,16 +1,14 @@
 module.exports = {
-  npm: {/*
-    globals: {
-      ajax: '@fdaciuk/ajax',
-      marked: 'marked'
-    }*/
-  },
   files: {
     javascripts: {joinTo: 'app.js'},
     stylesheets: {joinTo: 'app.css'}
   },
   plugins: {
     babel: { presets: ['latest'] },
+    closurecompiler: {
+      compilationLevel: 'SIMPLE',
+      createSourceMap: true
+    },
     postcss: { processors: [require('autoprefixer')] },
     afterBrunch: [
       'npm run build:docs'
